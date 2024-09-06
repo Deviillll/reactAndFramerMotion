afeeff200fa21ec6cc03e7aba706d9d7a42e551f
Hammad Hussain <93250@gcslahore.edu.pk>
2024-09-06 19:12:55 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Header from "./Componenets/Header";
 import About from "./Componenets/About";
@@ -7,14 +7,10 @@ import Contact from "./Componenets/Contact";
 import Projects from "./Componenets/Projects.tsx";
 import AboutMe from "./Componenets/AboutMe.tsx";
 import CV from "./Componenets/CV.tsx";
-import MblProjContainer from "./Componenets/MblProjContainer.tsx";
-import Lenis from "lenis";
 
-import Marque from "./Componenets/Marque.tsx";
 import Skills from "./Componenets/Skills.tsx";
 import ContactComponent from "./Componenets/ContactNow.tsx";
 import FooterComponent from "./Componenets/Footer.tsx";
-import Testing from "./Componenets/Testing.tsx";
 
 const App = () => {
   return (
